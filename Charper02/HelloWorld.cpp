84f6222a0ba89ab264f518188e1c024a970a19d2
unknown <kobe.0823@foxmail.com>
2020-06-05 00:36:26 +0800

    chy

---
+++ Charper02/HelloWorld.cpp
@@ -0,0 +1,12 @@
+//
+// Created by kobe0 on 2020/6/5.
+//
+
+#include <iostream>
+#include "HelloWorld.h"
+
+int main()
+{
+    std::cout << "Hello,world!" << std::endl;
+    std::cout << "First C++ code!" << std::endl;
+}
